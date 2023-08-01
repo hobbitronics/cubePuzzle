d50af0b902f9e61a716b6ea8a1aec107b458e398
Michael Wilson <miguel.dw@gmail.com>
2023-08-01 21:07:05 +0930

    add a cube and rotate with mouse

--- main.js
+++ main.js
@@ -2,5 +2,47 @@ import * as THREE from "three";
 
 //scene
 const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  45,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 
-//create a sphere
+const renderer = new THREE.WebGLRenderer({});
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const light = new THREE.PointLight(0xffffff, 1, 100);
+const cube = new THREE.Mesh(geometry, material);
+
+camera.position.z = 5;
+renderer.setSize(window.innerWidth, window.innerHeight);
+light.position.set(0, 10, 10);
+scene.add(light);
+scene.add(cube);
+
+document.body.appendChild(renderer.domElement);
+
+// function animate() {
+//   requestAnimationFrame(animate);
+//   cube.rotation.x += Math.random() * 0.1;
+//   cube.rotation.y += 0.01;
+renderer.render(scene, camera);
+// }
+// animate();
+
+//rotate the camera when the mouse moves
+function onMouseMove(event) {
+  let x = 0;
+  let y = 0;
+  x = event.clientX;
+  y = event.clientY;
+  cube.rotation.x = y * 0.01;
+  cube.rotation.y = x * -0.01;
+  renderer.render(scene, camera);
+}
+const onClick = () => window.addEventListener("mousemove", onMouseMove, false);
+const onRelease = () =>
+  window.removeEventListener("mousemove", onMouseMove, false);
+window.addEventListener("mousedown", onClick, false);
+window.addEventListener("mouseup", onRelease, false);
