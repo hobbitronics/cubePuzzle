1e5195fb549d3d74e884b1562925ec511f579956
Aaron Wilson <aaronwilson@Aarons-iMac.local>
2023-08-01 12:55:29 -0600

    add cube and orthographic camera

--- main.js
+++ main.js
@@ -1,25 +1,22 @@
 import * as THREE from "three";
 
+const loader = new THREE.ObjectLoader();
+
+
 //scene
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(
-  45,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
+const camera = new THREE.OrthographicCamera(
+  -1, 1, 1, -1, 1, 1000
 );
 
+let cube;
 const renderer = new THREE.WebGLRenderer({});
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const light = new THREE.PointLight(0xffffff, 1, 100);
-const cube = new THREE.Mesh(geometry, material);
 
 camera.position.z = 5;
 renderer.setSize(window.innerWidth, window.innerHeight);
 light.position.set(0, 10, 10);
 scene.add(light);
-scene.add(cube);
 
 document.body.appendChild(renderer.domElement);
 
@@ -51,6 +48,7 @@ function onTouchMove(event) {
   cube.rotation.y = x * -0.01;
   renderer.render(scene, camera);
 }
+
 const onClick = () => window.addEventListener("mousemove", onMouseMove, false);
 const onRelease = () =>
   window.removeEventListener("mousemove", onMouseMove, false);
@@ -59,7 +57,30 @@ const onTouchStart = () =>
 const onTouchEnd = () =>
   window.removeEventListener("touchmove", onTouchMove, false);
 
-window.addEventListener("mousedown", onClick, false);
-window.addEventListener("mouseup", onRelease, false);
-window.addEventListener("touchstart", onTouchStart, false);
-window.addEventListener("touchend", onTouchEnd, false);
+loader.load(
+  "model.json",
+
+  // onLoad callback
+  function (obj) {
+    cube = obj
+    // Add the loaded object to the scene
+    scene.add(cube);
+    renderer.render(scene, camera);
+
+    window.addEventListener("mousedown", onClick, false);
+    window.addEventListener("mouseup", onRelease, false);
+    window.addEventListener("touchstart", onTouchStart, false);
+    window.addEventListener("touchend", onTouchEnd, false);
+  },
+
+  // onProgress callback
+  function (xhr) {
+    console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+  },
+
+  // onError callback
+  function (err) {
+    console.error('An error happened');
+  }
+);
+
